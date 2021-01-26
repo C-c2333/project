bbe77311c8537e985510a8c8526c4cc4ff8995ec
522407783@qq.com <12345678-+>
2021-01-26 09:55:48 +0800

    new java

---
+++ src/main/java/test.java
@@ -0,0 +1,15 @@
+/**
+ * @Author cdl
+ * @Date 2021/1/26 9:55
+ * @Param $
+ * @Return $
+ */
+
+/**
+ *@ClassName test
+ *@Description TODO
+ *@Author cdl
+ *@Date 2021/1/26 9:55
+ **/
+public class test {
+}
