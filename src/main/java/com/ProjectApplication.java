ecc117253b1c7df9c473b06f4ba5e99e13af1490
522407783@qq.com <522407783@qq.com>
2021-05-03 12:52:24 +0800

    mybatis+

--- src/main/java/com/ProjectApplication.java
+++ src/main/java/com/ProjectApplication.java
@@ -1,9 +1,14 @@
 package com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@MapperScan("com.pj.mybatisPlus.dao")
+@EnableAutoConfiguration
 public class ProjectApplication {
 
     public static void main(String[] args) {
